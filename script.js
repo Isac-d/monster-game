acfc48535b142a4fe1292d3c807511d50b2d34b1
sesar <54498315+sesar2@users.noreply.github.com>
2025-03-19 23:56:08 +0100

    js styling and small logic implementations

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ let monsterList = [];
 let logArray = [];
 let activeMonster;
 
+let isOpen = false
+
 const energyButton = document.querySelector(".energy-btn");
 const playButton = document.querySelector(".play-btn");
 const feedButton = document.querySelector(".feed-btn");
+const addCard = document.querySelector('.div-add-new-monster-card')
+const overlay = document.querySelector('.overlay')
 
 const addbutton = document.querySelector(".add-btn");
 
@@ -27,9 +31,9 @@ function Monster(name, monsterType) {
       this.energy = Math.max(this.energy - 10, 0);
     }
 
-    logArray.push(`You gave ${this.name} some food`)
+    logArray.push(`You gave ${this.name} some food!`);
+    renderLog();
     renderMonster();
-
   };
 
   this.rest = function () {
@@ -37,62 +41,100 @@ function Monster(name, monsterType) {
     this.happiness = Math.max(this.happiness - 10, 0);
     this.energy = Math.min(this.energy + 40, 100);
 
-    logArray.push(`${this.name} took a nap`)
+    logArray.push(`${this.name} took a nap!`);
+    renderLog();
     renderMonster();
-
   };
   this.play = function () {
     this.fullness = Math.max(this.fullness - 10, 0);
-    
+
     this.happiness = Math.min(this.happiness + 30, 100);
-    
+
     this.energy = Math.max(this.energy - 10, 0);
 
-    logArray.push(`You played with ${this.name}`)
+    logArray.push(`You played with ${this.name}!`);
+    renderLog();
     renderMonster();
   };
-  
-  };
+}
 
 const renderMonster = () => {
-  //   const monsterName = document.querySelector(".monster-name");
+
+  if (activeMonster.energy === 0 || activeMonster.happiness === 0 || activeMonster.fullness === 0) {
+    logArray.push(`${activeMonster.name} ran away!`);
+    
+    monsterList = monsterList.filter(monster => monster !== activeMonster);
+
+    activeMonster = monsterList.length > 0 ? monsterList[0] : null;
+
+    renderMonsterList();
+    renderLog();
+
+    if (!activeMonster) return;
+  }
+
+
+
+  const monsterName = document.getElementById("monster-name");
   const monsterType = document.querySelector(".monster-type");
   const monsterEnergy = document.querySelector(".energy");
   const monsterHunger = document.querySelector(".hunger");
   const monsterHappiness = document.querySelector(".happiness");
   const monsterImage = document.querySelector(".monster-image");
 
+  monsterName.value = activeMonster.name
   monsterType.innerHTML = activeMonster.monsterType;
   monsterEnergy.innerHTML = activeMonster.energy;
   monsterHunger.innerHTML = activeMonster.fullness;
   monsterHappiness.innerHTML = activeMonster.happiness;
 
   let type = activeMonster.monsterType.toLowerCase();
-
-  monsterImage.src =
+  if(monsterList.length == 0){
+    monsterImage.src = '/images/dcxehfe-dd22d80d-4cff-49bf-be56-bb51f5ea0a78.gif'
+  }else{
+    monsterImage.src =
     type === "dragon"
-      ? "/images/dragon.png"
-      : type === "golem"
-      ? "/images/golem.png"
-      : type === "troll"
-      ? "/images/troll.png"
-      : "/images/unknown.png";
+    ? "/images/dragon.png"
+    : type === "golem"
+    ? "/images/golem.png"
+    : type === "troll"
+    ? "/images/troll.png"
+    : "/images/unknown.png";
+  }
 };
 
 const renderMonsterList = () => {
+  const monsterImage = document.querySelector(".monster-image");
+
   const monsterSelect = document.getElementById("monster-name");
   const monsterType = document.querySelector(".monster-type");
   const noMonsterText = document.querySelector(".no-monster");
+  const statDiv = document.querySelector(".stat-container");
+  const activityButton = document.querySelectorAll('.monster-btn')
   monsterSelect.innerHTML = "";
 
   if (monsterList.length === 0) {
+    activityButton.forEach(button => {
+      button.disabled = true
+      button.style.filter = 'opacity(50%)'
+      button.style.cursor = 'default'
+    });
+    monsterImage.src = '/images/dcxehfe-dd22d80d-4cff-49bf-be56-bb51f5ea0a78.gif';
     monsterType.style.display = "none";
     monsterSelect.style.display = "none";
-
+    statDiv.style.display = "none";
     noMonsterText.style.display = "block";
+
   } else {
+
+      activityButton.forEach(button => {
+      button.disabled = false
+      button.style.filter = 'none'
+      button.style.cursor = 'pointer'
+    });
     monsterType.style.display = "block";
     monsterSelect.style.display = "block";
+    statDiv.style.display = "flex";
     noMonsterText.style.display = "none";
 
     monsterList.forEach((monster) => {
@@ -105,20 +147,35 @@ const renderMonsterList = () => {
 };
 
 const addNewMonster = () => {
+  const inputValue = document.getElementById("name"); 
+
   if (monsterList.length > 3) {
     alert("stop");
-  } else {
-    const monsterName = document.getElementById("name").value;
-    const monsterType = document.getElementById("type").value;
-
-    let newMonster = new Monster(monsterName, monsterType);
-    monsterList.push(newMonster);
-    activeMonster = monsterList[monsterList.length - 1];
-    renderMonsterList();
-    renderMonster();
+    return;
   }
-  console.log(monsterList);
+
+  const monsterName = inputValue.value.trim(); 
+  const monsterType = document.getElementById("type").value;
+
+  if (monsterName.length === 0) {
+    alert("Name cannot be empty");
+    return;
+  }
+
+  let newMonster = new Monster(monsterName, monsterType);
+  monsterList.push(newMonster);
+  activeMonster = monsterList[monsterList.length - 1];
+
+  inputValue.value = ''; 
+
+  renderMonsterList();
+  renderMonster();
+
+  addCard.style.display = "none";
+  overlay.style.display = "none";
+
 };
+
 
 const handleChangeMonster = () => {
   const selectedMonsterName = document.getElementById("monster-name").value;
@@ -132,8 +189,47 @@ const handleChangeMonster = () => {
   }
 };
 
+const renderLog = () => {
+  const logDiv = document.querySelector(".log");
+  logDiv.innerHTML = "";
+  const reversedArr = logArray.reverse()
+  logArray.forEach((log) => {
+    
+    const logItem = document.createElement("div");
+    logItem.classList.add("log-item");
+    logItem.innerHTML = log;
 
+    if (log.includes("food")) {
+      logItem.classList.add("log-feed");
+    } else if (log.includes("played")) {
+      logItem.classList.add("log-play");
+    } else if (log.includes("nap")) {
+      logItem.classList.add("log-rest");
+    } else if (log.includes("ran")){
+      logItem.classList.add('log-ranaway')
+    }
 
+    logDiv.appendChild(logItem);
+  });
+};
+
+const handleToggle = () => {
+  if(!isOpen){
+    addCard.style.display = 'flex'
+    overlay.style.display = 'block'
+  }else{
+    addCard.style.display = 'none'
+    overlay.style.display = 'none'
+  }
+isOpen = !isOpen
+ 
+}
+
+const addNewButton = document.querySelector('.add-new')
+if(overlay){
+  overlay.addEventListener('click', handleToggle)
+}
+addNewButton.addEventListener('click', handleToggle)
 feedButton.addEventListener("click", () => activeMonster.feed());
 playButton.addEventListener("click", () => activeMonster.play());
 energyButton.addEventListener("click", () => activeMonster.rest());
