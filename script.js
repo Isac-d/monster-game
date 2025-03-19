77ed69a257b6519e0193d654b2f944746825bf6e
sesar <54498315+sesar2@users.noreply.github.com>
2025-03-19 16:59:25 +0100

    buttons change stats of monster

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let startValue = 50;
 let monsterList = [];
+let logArray = [];
 let activeMonster;
+
+const energyButton = document.querySelector(".energy-btn");
+const playButton = document.querySelector(".play-btn");
+const feedButton = document.querySelector(".feed-btn");
+
+const addbutton = document.querySelector(".add-btn");
 
 function Monster(name, monsterType) {
   this.name = name;
@@ -10,22 +17,45 @@ function Monster(name, monsterType) {
   this.fullness = startValue;
 
   this.feed = function () {
-    this.fullness += 10;
-    
+    if (this.fullness < 100) {
+      this.fullness = Math.min(this.fullness + 30, 100);
+    }
+    if (this.happiness < 100) {
+      this.happiness = Math.min(this.happiness + 5, 100);
+    }
+    if (this.energy > 0) {
+      this.energy = Math.max(this.energy - 10, 0);
+    }
+
+    logArray.push(`You gave ${this.name} some food`)
+    renderMonster();
+
   };
 
   this.rest = function () {
-    this.energy += 10;
+    this.fullness = Math.max(this.fullness - 10, 0);
+    this.happiness = Math.max(this.happiness - 10, 0);
+    this.energy = Math.min(this.energy + 40, 100);
+
+    logArray.push(`${this.name} took a nap`)
+    renderMonster();
+
+  };
+  this.play = function () {
+    this.fullness = Math.max(this.fullness - 10, 0);
     
+    this.happiness = Math.min(this.happiness + 30, 100);
+    
+    this.energy = Math.max(this.energy - 10, 0);
+
+    logArray.push(`You played with ${this.name}`)
+    renderMonster();
+  };
+  
   };
 
-  this.playWith = function () {
-    this.happiness += 10;
-    
-  };
-}
 const renderMonster = () => {
-//   const monsterName = document.querySelector(".monster-name");
+  //   const monsterName = document.querySelector(".monster-name");
   const monsterType = document.querySelector(".monster-type");
   const monsterEnergy = document.querySelector(".energy");
   const monsterHunger = document.querySelector(".hunger");
@@ -50,57 +80,66 @@ const renderMonster = () => {
 };
 
 const renderMonsterList = () => {
-    const monsterSelect = document.getElementById("monster-name");
-    const noMonsterText = document.querySelector('.no-monster')
-    monsterSelect.innerHTML = "";
+  const monsterSelect = document.getElementById("monster-name");
+  const monsterType = document.querySelector(".monster-type");
+  const noMonsterText = document.querySelector(".no-monster");
+  monsterSelect.innerHTML = "";
 
-    if(monsterList.length === 0){
-        monsterSelect.style.display = 'none'
-        noMonsterText.style.display = 'block'
+  if (monsterList.length === 0) {
+    monsterType.style.display = "none";
+    monsterSelect.style.display = "none";
 
-    }else{   
-        monsterSelect.style.display = 'block'
-        noMonsterText.style.display = 'none'
+    noMonsterText.style.display = "block";
+  } else {
+    monsterType.style.display = "block";
+    monsterSelect.style.display = "block";
+    noMonsterText.style.display = "none";
 
-        monsterList.forEach((monster) => {
-            const monsterOption = document.createElement("option");
-            monsterOption.value = monster.name;
-            monsterOption.innerHTML = monster.name;
-            monsterSelect.appendChild(monsterOption);
-        });
-    }
+    monsterList.forEach((monster) => {
+      const monsterOption = document.createElement("option");
+      monsterOption.value = monster.name;
+      monsterOption.innerHTML = monster.name;
+      monsterSelect.appendChild(monsterOption);
+    });
+  }
 };
 
 const addNewMonster = () => {
-    if(monsterList.length > 3) {
-        alert('stop')
-    }else{
+  if (monsterList.length > 3) {
+    alert("stop");
+  } else {
+    const monsterName = document.getElementById("name").value;
+    const monsterType = document.getElementById("type").value;
 
-        const monsterName = document.getElementById("name").value;
-        const monsterType = document.getElementById("type").value;
-        
-        let newMonster = new Monster(monsterName, monsterType);
-        monsterList.push(newMonster);
-        activeMonster = monsterList[0]
-        renderMonsterList();
-        renderMonster()
-        
-    }
+    let newMonster = new Monster(monsterName, monsterType);
+    monsterList.push(newMonster);
+    activeMonster = monsterList[monsterList.length - 1];
+    renderMonsterList();
+    renderMonster();
+  }
   console.log(monsterList);
 };
 
 const handleChangeMonster = () => {
-    const selectedMonsterName =  document.getElementById("monster-name").value;
+  const selectedMonsterName = document.getElementById("monster-name").value;
 
-    activeMonster = monsterList.find(monster => monster.name === selectedMonsterName);
+  activeMonster = monsterList.find(
+    (monster) => monster.name === selectedMonsterName
+  );
 
-    if (activeMonster) {
-        renderMonster();
-    }
-}
+  if (activeMonster) {
+    renderMonster();
+  }
+};
 
-const addbutton = document.querySelector(".add-btn");
+
+
+feedButton.addEventListener("click", () => activeMonster.feed());
+playButton.addEventListener("click", () => activeMonster.play());
+energyButton.addEventListener("click", () => activeMonster.rest());
 addbutton.addEventListener("click", addNewMonster);
-document.getElementById("monster-name").addEventListener("change", handleChangeMonster);
+document
+  .getElementById("monster-name")
+  .addEventListener("change", handleChangeMonster);
 
 renderMonsterList();
